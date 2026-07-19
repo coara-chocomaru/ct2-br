79285fb709553c5cff45f3a0492c9bc3ea004d82
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-19 11:12:39 +0900

    Rename product makefiles and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a03br.mk
+    $(LOCAL_DIR)/cm_a03br.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a03br-eng \
-    lineage_a03br-userdebug
+    cm_a03br-eng \
+    cm_a03br-userdebug
