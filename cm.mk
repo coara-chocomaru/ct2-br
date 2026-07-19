e07e85e98da304db1159fc7127c0858fef726d3f
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-19 10:31:58 +0900

    Delete cm.mk

--- cm.mk
+++
@@ -1,4 +0,0 @@
-# Inherit device configuration
-$(call inherit-product, device/sts/a03br/lineage_a03br.mk)
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
-TARGET_LOCALES := ja_JP en_US en_AU
