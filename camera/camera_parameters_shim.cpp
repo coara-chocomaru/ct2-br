5c34c30b907cb590f7f9950497da0c9459e2a9ca
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-23 08:14:27 +0900

    Create camera_parameters_shim.cpp

---
+++ camera/camera_parameters_shim.cpp
@@ -0,0 +1,8 @@
+#include <stdint.h>
+extern "C" {
+
+__attribute__((visibility("default")))
+const int android_CameraParameters_PIXEL_FORMAT_YUV420P_I420E
+    __asm__("_ZN7android16CameraParameters25PIXEL_FORMAT_YUV420P_I420E") = 0;
+
+}  // extern "C"
