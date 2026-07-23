56542227fe496ebef14cb4e259646d08fafe373a
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-23 18:32:49 +0900

    Update Android.mk for libcamera_parameters_shim

--- camera/Android.mk
+++ camera/Android.mk
@@ -3,8 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libcamera_parameters_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := camera_parameters_shim.cpp
-LOCAL_CFLAGS := -Wall -Werror -fvisibility=hidden -Wno-error=attributes
-LOCAL_CPPFLAGS := -std=gnu++11
-LOCAL_LDFLAGS := -Wl,--exclude-libs,ALL
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
+LOCAL_CFLAGS := -Wall -Wextra -fvisibility=default
+LOCAL_PRELINK_MODULE := false
 include $(BUILD_SHARED_LIBRARY)
