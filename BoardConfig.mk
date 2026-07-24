009206c87855c37f47df88487b808a238f93582c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-24 10:51:32 +0900

    Update SELinux policies in BoardConfig.mk
    Added SELinux policy files to BOARD_SEPOLICY_UNION and updated BOARD_SEPOLICY_DIRS.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -205,8 +205,80 @@ TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
 include vendor/sts/a03br/BoardConfigVendor.mk
 
 # SELinux
-#BOARD_SEPOLICY_DIRS := \
-	#$(DEVICE_PATH)/sepolicy/
+BOARD_SEPOLICY_DIRS := \
+	$(DEVICE_PATH)/sepolicy/
+
+BOARD_SEPOLICY_UNION += \
+    FMRadiod.te \
+    adbd.te \
+    asec_apk_file.te \
+    at_router.te \
+    atcmdsrv.te \
+    bluetooth.te \
+    bootanim.te \
+    bsp_marvell.mk \
+    cache_file.te \
+    codecIPC_server.te \
+    core_compressor.te \
+    debuggerd.te \
+    device.te \
+    diag.te \
+    drmserver.te \
+    dumpstate.te \
+    eeh.te \
+    env.te \
+    file.te \
+    file_contexts \
+    global_macros \
+    hawk.te \
+    hdcp.te \
+    healthd.te \
+    iml.te \
+    imsc.te \
+    init.te \
+    installd.te \
+    kernel.te \
+    keystore.te \
+    kmsg.te \
+    lmkd.te \
+    load_g_cali_data.te \
+    log_on_boot.te \
+    marvelltel.te \
+    mediaserver.te \
+    memsicd.te \
+    memsicp.te \
+    mned.te \
+    mrvl_gpsd.te \
+    mwirelessd.te \
+    netd.te \
+    nfc.te \
+    nvm.te \
+    phservice.te \
+    platform_app.te \
+    pppmodem.te \
+    radio.te \
+    recovery.te \
+    rild.te \
+    root_detect.te \
+    run_hawk_on_boot.te \
+    sdcardd.te \
+    seapp_contexts \
+    service_contexts \
+    servicemanager.te \
+    setup_fs.te \
+    shell.te \
+    smartcard.te \
+    surfaceflinger.te \
+    system_app.te \
+    system_server.te \
+    teecsstdca.te \
+    touch_updater.te \
+    ueventd.te \
+    untrusted_app.te \
+    vdc.te \
+    vold.te \
+    wpa.te \
+    zygote.te
 
 BOARD_SECCOMP_POLICY += $(DEVICE_PATH)/seccomp_policy
 TARGET_SELINUX_ENFORCE := false
