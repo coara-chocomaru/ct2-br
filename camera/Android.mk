eba920bc1b8f478722169380e14ce98cfa3e3b31
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-28 18:25:14 +0900

    Update Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,4 +7,5 @@ LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
 LOCAL_CFLAGS := -Wall -Wextra -fvisibility=default
 LOCAL_PRELINK_MODULE := false
 LOCAL_MULTILIB := both
+
 include $(BUILD_SHARED_LIBRARY)
