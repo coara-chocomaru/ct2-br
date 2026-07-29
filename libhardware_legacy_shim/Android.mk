aed79df6ddb5d9d02e97dd474d99d1de5f797747
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-30 01:10:54 +0900

    Add -fPIC flag to LOCAL_CFLAGS in Android.mk

--- libhardware_legacy_shim/Android.mk
+++ libhardware_legacy_shim/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libhardware_legacx
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := libhardware_legacy_shim.c
-LOCAL_CFLAGS := -Wall -Werror -fvisibility=hidden -O2
+LOCAL_CFLAGS := -Wall -Werror -fvisibility=hidden -O2 -fPIC
 LOCAL_SHARED_LIBRARIES := libdl liblog
 LOCAL_MULTILIB := both
 
