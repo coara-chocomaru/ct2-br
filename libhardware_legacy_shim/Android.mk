9dea84fce5f45be556546c768337320b890ec396
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-30 00:22:07 +0900

    Update Android.mk

--- libhardware_legacy_shim/Android.mk
+++ libhardware_legacy_shim/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libhardware_legacy_shim
+LOCAL_MODULE := libhardware_legacx
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := libhardware_legacy_shim.c
 LOCAL_CFLAGS := -Wall -Werror -fvisibility=hidden -O2
