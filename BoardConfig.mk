fdbd9918c3def12fb9966f1ad12271ea78a76cbb
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-30 23:54:01 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,15 +62,14 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_MRVL := true
 
 BOARD_WLAN_VENDOR := MRVL
-WIFI_DRIVER_MODULE_PATH := "/lib/modules/sd8801.ko"
-WIFI_DRIVER_MODULE_NAME	:= "sd8801"
-WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/firmware/mrvl/sd8801_uapsta.bin cfg80211_wext=12 sta_name=wlan uap_name=wlan wfd_name=p2p fw_name=mrvl/sd8801_uapsta.bin"
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/sd8801/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/mrvl/sd8801_uapsta.bin"
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/firmware/mrvl/sd8801_uapsta.bin"
-WIFI_DRIVER_FW_PATH_P2P := "/system/etc/firmware/mrvl/sd8801_uapsta.bin"
-WIFI_SDIO_IF_DRIVER_MODULE_PATH := "/lib/modules/mlan8801.ko"
-WIFI_SDIO_IF_DRIVER_MODULE_NAME := "mlan8801"
+WIFI_DRIVER_MODULE_PATH := "/lib/modules/sd8887.ko"
+WIFI_DRIVER_MODULE_NAME := "sd8887"
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/sd8887/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/mrvl/sd8887_uapsta.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/firmware/mrvl/sd8887_uapsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/etc/firmware/mrvl/sd8887_uapsta.bin"
+WIFI_SDIO_IF_DRIVER_MODULE_PATH := "/lib/modules/mlan8887.ko"
+WIFI_SDIO_IF_DRIVER_MODULE_NAME := "mlan8887"
 WIFI_SDIO_IF_DRIVER_MODULE_ARG := ""
 MRVL_WIRELESS_DAEMON_API := true
 
