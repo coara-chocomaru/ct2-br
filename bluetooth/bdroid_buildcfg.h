e0db9f0d2b409b53ebb288aa4c399f6c1a259263
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-04 03:13:15 +0900

    Update bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,18 +1,9 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME               "PXT1928 Tablet"
-#define HCI_INCLUDED                     TRUE
-#define MRVL_BT_DEFAULT_BAUD             3000000
-#define MRVL_BT_FIXED_BAUD               TRUE
-#define BT_WAKE_VIA_USERIAL_IOCTL        TRUE
-#define BTM_ACL_BUFFER_SIZE              1024
-#define BTM_ACL_BUFFER_MAX               200
-#define BTM_DEVICE_DB_MAX                100
-#define BTM_SCO_HCI_ERR_NO_ESCO_SUPPORT  TRUE
-#define BT_DEFAULT_SCO_ROUTE_SCO         1
-#define BTM_SCO_ENABLE_PCM_TX_RX         TRUE
-#define LPM_IDLE_TIMEOUT_MULTIPLE        5
-#define BTIF_DM_RECONNECT_ADD_DELAY_MS   1000
-#define BLE_VND_INCLUDED                 TRUE
-#define BTE_APPL_TRACE_LEVEL             TRACE_LEVEL_INFO
+#define BTM_DEF_LOCAL_NAME "PXA1928"
+#define MAX_ACL_CONNECTIONS 7
+#define MAX_L2CAP_CHANNELS 16
+#define BLE_VND_INCLUDED TRUE
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
 #endif
