46c927242e5356766ac30e2737a4cb984bf7644c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-06 16:52:28 +0900

    Add Android.mk for perfd executable build

---
+++ fixsys/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := perfd
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES := perfd.cpp
+LOCAL_CFLAGS := -Wall -Wextra -O2
+LOCAL_CPPFLAGS := -std=c++11
+LOCAL_SHARED_LIBRARIES := liblog libc
+LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
+include $(BUILD_EXECUTABLE)
