522040cec28c224bda3e5a2bc5a2448302aafb3b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-07 11:16:22 +0900

    Update Android.mk

--- fixsys/Android.mk
+++ fixsys/Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES := perfd.cpp
 LOCAL_CFLAGS := -Wall -Wextra -O2
 LOCAL_CPPFLAGS := -std=c++11
-LOCAL_SHARED_LIBRARIES := liblog libc libc++
+LOCAL_SHARED_LIBRARIES := liblog libc libstlport
+LOCAL_C_INCLUDES += external/stlport/stlport
 LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
 include $(BUILD_EXECUTABLE)
