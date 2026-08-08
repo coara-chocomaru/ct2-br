be756b3371768ebd16fa1936c7278dcd3e261ffc
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-09 08:15:37 +0900

    Remove LOCAL_PRELINK_MODULE from Android.mk

--- ioe-test/Android.mk
+++ ioe-test/Android.mk
@@ -5,6 +5,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := camera_ion_shim.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_CFLAGS := -Wall -Wextra -fvisibility=default
-LOCAL_PRELINK_MODULE := false
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
