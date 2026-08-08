de82a610770f959c3a972978fe1e7d0e1af7ba7b
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-08-09 08:17:59 +0900

    Update Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -5,7 +5,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := camera_parameters_shim.cpp
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libdl
 LOCAL_CFLAGS := -Wall -Wextra -fvisibility=default
-LOCAL_PRELINK_MODULE := false
 LOCAL_MULTILIB := both
-
 include $(BUILD_SHARED_LIBRARY)
